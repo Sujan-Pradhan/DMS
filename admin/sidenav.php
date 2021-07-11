08d21fb015b8fa6497a45794469749c4c58b9997
Sujan <lamdalimbu@gmail.com>
2021-07-11 23:43:43 +0545

    Added all the files

---
+++ admin/sidenav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+		<!--Sidenav-->
+				<div class="list-group bg-info" style="height: 90vh;">
+					<a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Dashboard</a>
+					<a href="admin.php" class="list-group-item list-group-item-action bg-info text-center text-white">Administrator</a>
+					<a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Doctor</a>
+					<a href="" class="list-group-item list-group-item-action bg-info text-center text-white">Patient</a>
+				</div>
+
+				<!-- Ends -->
+
+
+</body>
+</html>
